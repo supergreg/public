4cd536cf878b041f2fdf3701615ce30915a0db4b
Gregor Janus <Gregor Janus@acer.fritz.box>
2014-03-17 22:17:44 +0100

    weiterentwicklung

---
+++ Testprojekt/src/ein/pack/CommonUtils.java
@@ -0,0 +1,24 @@
+package ein.pack;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/**
+ * $HeadURL: $
+ * Copyright: 2012, Truxxmobile
+ * Created by: gjanus
+ * $Id: $
+ * $Revision: $
+ * $Date: $
+ * $Author: $
+ * 
+ * Description:
+ */
+public class CommonUtils
+{
+
+  public static String getText(String prefix)
+  {
+    return prefix + "Text";
+  }
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+}
--- Testprojekt/src/ein/pack/Main.java
+++ Testprojekt/src/ein/pack/Main.java
@@ -5,6 +5,19 @@ public class Main
   public static void main(String[] args)
   {
     System.out.println("Ein Test");
+    CommonUtils.getText("ZZZZ");
     TestUtils.printText("Hallo");
+    doNothing();
+  }
+
+  private static void doNothing()
+  {
+    try
+    {
+      Thread.sleep(500);
+    } catch (InterruptedException e)
+    {
+      e.printStackTrace();
+    }
   }
 }
